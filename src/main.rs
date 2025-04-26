e41582b406e0bb21d83c3c2327206448bf67f939
smehlhoff <stevemehlhoff@gmail.com>
2025-04-26 11:52:46 -0700

    Fix stuff

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ struct Client {
 }
 
 impl Client {
-    fn new(addr: SocketAddr) -> Self {
+    const fn new(addr: SocketAddr) -> Self {
         Self {
             nick: String::new(),
             addr,
@@ -59,7 +59,7 @@ impl Clients {
     }
 }
 
-async fn handle_line(nick: String, line: String) -> io::Result<String> {
+async fn handle_line(nick: &str, line: &str) -> io::Result<String> {
     // let tokens: Vec<&str> = line.trim().split(" ").collect();
 
     // println!("{:?}", tokens);
@@ -80,64 +80,90 @@ async fn handle_client(
     let mut reader = BufReader::new(reader);
     let mut client = Client::new(addr);
 
-    println!("Client connected from {:?}", addr);
+    println!("notice: client connected from {:?}", addr);
 
     writer
-        .write_all(b"Welcome! Please enter your nick below\n")
+        .write_all(b"server: please enter your nick below\n")
         .await?;
 
     loop {
         let mut nick = String::new();
         let bytes = reader.read_line(&mut nick).await?;
 
-        nick = nick.trim().to_string();
-
-        if nick.is_ascii() && !nick.is_empty() {
-            if clients.check_nick(&nick).await == false {
-                client.set_nick(nick);
-                break;
-            }
-            writer.write_all(b"This nick is already taken\n").await?;
-        } else {
-            writer.write_all(b"Please enter a valid nick\n").await?;
-        }
-
         if bytes == 0 {
             clients.remove(addr).await;
 
-            println!("Client disconnected from {:?}", addr);
+            println!("notice: client disconnected from {:?}", addr);
             println!("{:?}", clients.connections);
 
             break;
         }
+
+        nick = nick.trim().to_string();
+
+        if nick.is_ascii() && !nick.is_empty() {
+            if nick.len() < 3 {
+                writer.write_all(b"server: nick is too short\n").await?;
+            } else if nick.len() > 15 {
+                writer.write_all(b"server: nick is too long\n").await?;
+            } else {
+                if clients.check_nick(&nick).await == false {
+                    client.set_nick(nick);
+                    break;
+                } else {
+                    writer
+                        .write_all(b"server: this nick is already taken\n")
+                        .await?;
+                }
+            }
+        } else {
+            writer
+                .write_all(b"server: please enter a valid nick\n")
+                .await?;
+        }
     }
 
     clients.add(client.clone()).await;
+
+    match tx.send(format!("server: {} has joined the server!\n", client.nick)) {
+        Ok(_) => {}
+        Err(e) => println!("Unable to send line: {}", e),
+    }
 
     loop {
         let mut input = String::new();
 
         tokio::select! {
             bytes = reader.read_line(&mut input) => {
-                let line = handle_line(client.nick.clone(), input).await?;
-
-                match tx.send(line) {
-                    Ok(_) => {},
-                    Err(e) => println!("Unable to send line: {}", e)
-                }
-
                 if bytes.unwrap() == 0 {
                     clients.remove(addr).await;
 
-                    println!("Client disconnected from {:?}", addr);
+                    println!("notice: client disconnected from {:?}", addr);
                     println!("{:?}", clients.connections);
 
+                    match tx.send(format!("server: {} has left the server!\n", client.nick)) {
+                        Ok(_) => {}
+                        Err(e) => println!("Unable to send line: {}", e),
+                    }
+
                     break Ok(());
+                }
+
+                let line = handle_line(&client.nick, &input).await?;
+
+                if input.len() > 1 {
+                    match tx.send(line.to_string()) {
+                        Ok(_) => {},
+                        Err(e) => println!("Unable to send line: {}", e)
+                        }
                 }
             }
 
             msg = rx.recv() => {
-                writer.write_all(msg.unwrap().as_bytes()).await?;
+                match msg {
+                    Ok(msg) => writer.write_all(msg.as_bytes()).await?,
+                    Err(e) => println!("Unable to read line: {}", e)
+                }
             }
         }
     }
